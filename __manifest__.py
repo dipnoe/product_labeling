b2143e2dd718da92c1b8773d9114d48e01061678
Vagner Elena <dipnoe@gmail.com>
2023-12-16 03:33:37 +0700

    Initial commit of odoo module

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Product Labeling',
+    'version': '1.0',
+    'description': """Track the location and cost of products at any time""",
+    'category': 'Inventory',
+    # 'depends': ['base', 'product'],
+    # 'data': [
+    #     'security/ir.model.access.csv',
+    #     'views/product_label_view.xml',
+    #     'data/product_label_data.xml',
+    # ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
