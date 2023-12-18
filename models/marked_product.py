a99a5a28dba0e6733c8b75835e889631a51a0db3
Elena Vagner <develop.vagner@gmail.com>
2023-12-18 18:34:31 +0700

    feature: override create method for creating name with mark

--- models/marked_product.py
+++ models/marked_product.py
@@ -1,13 +1,23 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.custom_addons.utils.mark_generate import generate_random_mark
 
 
 class MarkedProduct(models.Model):
     _name = 'product_labeling.marked_product'
 
+    product = fields.Many2one('product_labeling.product', required=True)
+    name = fields.Char(name='Name', readonly=True, required=True)
     last_destination = fields.Text(string='Last destination', required=True)
-    status = fields.Selection([
+    state = fields.Selection([
         ('sale', 'Sale'),
         ('purchase', 'Purchase'),
         ('internal_movement', 'Internal movement')],
         string='Assigned status', required=True)
-    product = fields.Many2one('product_labeling.product', required=True)
+
+    @api.model
+    @api.depends('product')
+    def create(self, vals):
+        if vals.get('state') == 'purchase':
+            product_record = self.env['product_labeling.product'].browse(vals['product'])
+            vals['name'] = f'{product_record.name} #{generate_random_mark()}'
+        return super(MarkedProduct, self).create(vals)
